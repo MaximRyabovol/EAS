25e6d4e4811f483c11844f715e7c5c287c725ba1
Maxim Ryabovol <work.amur@gmail.com>
2019-07-13 10:56:57 +0900

    base

---
+++ lib/screens/membrs_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MembersListScreen extends StatefulWidget {
+  @override
+  _MembersListScreenState createState() => _MembersListScreenState();
+}
+
+class _MembersListScreenState extends State<MembersListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
